97a8734d40baf095f644b1bedfed851e614aa803
Jolaoluwa Amodu <jola47amodu@gmail.com>
2024-11-25 04:22:01 -0600

    Created admin page, initialized database

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import PostForm, EntranceForm
 from flask_ckeditor import CKEditor
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 ckeditor = CKEditor(app)
 app.config['SECRET_KEY'] = '5a340cc628ca0eb4c15fb7957130c63b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -20,6 +24,9 @@ def blog_page():
 @app.route('/admin')
 def admin_page():
     form = PostForm()
+    if form.validate_on_submit():
+        flash(f'Post Successfully uploaded!', 'success')
+        return redirect(url_for('blog_page'))
     return render_template('admin.html', form=form)
 
 
--- forms.py
+++ forms.py
@@ -7,7 +7,8 @@ from flask_ckeditor import CKEditorField
 class PostForm(FlaskForm):
     title = StringField('Title')
     content = CKEditorField('Content', validators=[DataRequired()])
-    media = StringField('Title')
+    media = FileField('Upload media')
+    submit = SubmitField('Upload')
 
 
 class EntranceForm(FlaskForm):
