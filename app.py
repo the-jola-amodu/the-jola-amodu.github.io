e5575eb5c94cc06f9a26a474752645055a0d8b65
Jolaoluwa Amodu <jola47amodu@gmail.com>
2024-11-25 00:52:39 -0600

    Template inheritance implemented

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, url_for
+from forms import PostForm, EntranceForm
+from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
+ckeditor = CKEditor(app)
+app.config['SECRET_KEY'] = '5a340cc628ca0eb4c15fb7957130c63b'
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/blog')
+def blog_page():
+    return render_template('blog.html')
+
+
+@app.route('/admin')
+def admin_page():
+    form = PostForm()
+    return render_template('admin.html', form=form)
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, FileField
+from wtforms.validators import DataRequired, Length, ValidationError
+from flask_ckeditor import CKEditorField
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title')
+    content = CKEditorField('Content', validators=[DataRequired()])
+    media = StringField('Title')
+
+
+class EntranceForm(FlaskForm):
+    pass_phrase = StringField('Pass Phrase', validators=[DataRequired()])
+
