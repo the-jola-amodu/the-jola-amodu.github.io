c2e0fd6b26c2244f27d30788ccebb3bf8a259cbe
Jolaoluwa Amodu <jola47amodu@gmail.com>
2024-12-08 16:29:38 -0600

    Set up environment variables

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask_ckeditor import CKEditor
 from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import find_dotenv, load_dotenv
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+
 
 app = Flask(__name__)
 ckeditor = CKEditor(app)
-app.config['SECRET_KEY'] = '5a340cc628ca0eb4c15fb7957130c63b'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
 db = SQLAlchemy(app)
 
